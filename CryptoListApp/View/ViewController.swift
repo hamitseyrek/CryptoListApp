c80cfb175dbb236cf569a1db6ac79cdef6f0a1f8
hamitseyrek <hamitseyrek@gmail.com>
2022-02-18 00:05:59 +0300

    add test banner to bottom with Google Admob

--- CryptoListApp/View/ViewController.swift
+++ CryptoListApp/View/ViewController.swift
@@ -6,11 +6,14 @@
 //
 
 import UIKit
+import GoogleMobileAds
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, GADBannerViewDelegate {
+    
     @IBOutlet weak var tableView: UITableView!
     private var cryptoListViewModel : CryptoListviewModel!
+    
+    var bannerView: GADBannerView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,9 +22,35 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         // Do any additional setup after loading the view.
         tableView.delegate = self
         tableView.dataSource = self
-        
+        // In this case, we instantiate the banner with desired ad size.
+        bannerView = GADBannerView(adSize: GADPortraitAnchoredAdaptiveBannerAdSizeWithWidth(300))
+        bannerView.adUnitID = "ca-app-pub-3940256099942544/2934735716"
+          bannerView.rootViewController = self
+        bannerView.delegate = self
+        bannerView.load(GADRequest())
+        addBannerViewToView(bannerView)
     }
-
+    
+    func addBannerViewToView(_ bannerView: GADBannerView) {
+        bannerView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(bannerView)
+        view.addConstraints(
+            [NSLayoutConstraint(item: bannerView,
+                                attribute: .bottom,
+                                relatedBy: .equal,
+                                toItem: view.safeAreaLayoutGuide,
+                                attribute: .top,
+                                multiplier: 1,
+                                constant: 0),
+             NSLayoutConstraint(item: bannerView,
+                                attribute: .centerX,
+                                relatedBy: .equal,
+                                toItem: view,
+                                attribute: .centerX,
+                                multiplier: 1,
+                                constant: 0)
+            ])
+    }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.cryptoListViewModel == nil ? 0 : cryptoListViewModel.numberOfRowsInSection()
     }
@@ -46,6 +75,8 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             }
         }
     }
-
+    
 }
 
+
+// ca-app-pub-3940256099942544/2934735716
