07ab9b80cf62ddd501a14c7db203c0b295f629f9
hamitseyrek <hamitseyrek@gmail.com>
2022-02-18 22:05:49 +0300

    add App Tracking Transparency for iOS 14+ privacy that Apple want for 3nd app like AdMob

--- CryptoListApp/View/ViewController.swift
+++ CryptoListApp/View/ViewController.swift
@@ -7,8 +7,9 @@
 
 import UIKit
 import GoogleMobileAds
+import AppTrackingTransparency
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, GADBannerViewDelegate {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, GADBannerViewDelegate, UIApplicationDelegate, UNUserNotificationCenterDelegate {
     
     @IBOutlet weak var tableView: UITableView!
     private var cryptoListViewModel : CryptoListviewModel!
@@ -23,14 +24,12 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         tableView.delegate = self
         tableView.dataSource = self
         // In this case, we instantiate the banner with desired ad size.
-        bannerView = GADBannerView(adSize: GADPortraitAnchoredAdaptiveBannerAdSizeWithWidth(300))
-        bannerView.adUnitID = "ca-app-pub-3940256099942544/2934735716"
-          bannerView.rootViewController = self
-        bannerView.delegate = self
-        bannerView.load(GADRequest())
-        addBannerViewToView(bannerView)
+        
+        //MARK: - App Tracking Transparency
+        NotificationCenter.default.addObserver(self, selector: #selector(requestIDFA), name: UIApplication.didBecomeActiveNotification, object: nil)
     }
     
+    //MARK: - Google Admob Configuration
     func addBannerViewToView(_ bannerView: GADBannerView) {
         bannerView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(bannerView)
@@ -39,7 +38,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
                                 attribute: .bottom,
                                 relatedBy: .equal,
                                 toItem: view.safeAreaLayoutGuide,
-                                attribute: .top,
+                                attribute: .bottom,
                                 multiplier: 1,
                                 constant: 0),
              NSLayoutConstraint(item: bannerView,
@@ -51,6 +50,33 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
                                 constant: 0)
             ])
     }
+    
+    //MARK: - App Tracking Transparency
+    @objc func requestIDFA() {
+        ATTrackingManager.requestTrackingAuthorization(completionHandler: { status in
+        // Tracking authorization completed. Start loading ads here.
+        // loadAd()
+          if status == .authorized {
+              print("auth")
+              
+              DispatchQueue.main.async {
+                  //MARK: - Google Admob Configuration
+                  self.bannerView = GADBannerView(adSize: GADPortraitAnchoredAdaptiveBannerAdSizeWithWidth(300))
+                  self.bannerView.adUnitID = "ca-app-pub-3940256099942544/2934735716"
+                  self.bannerView.rootViewController = self
+                  self.bannerView.delegate = self
+                  self.bannerView.load(GADRequest())
+                  self.addBannerViewToView(self.bannerView)
+              }
+              
+          } else if status == .denied {
+              print("denied")
+          } else {
+              print("else")
+          }
+      })
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.cryptoListViewModel == nil ? 0 : cryptoListViewModel.numberOfRowsInSection()
     }
